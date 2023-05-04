467f73f1054da8ef1f5cbe28c2e571115c1dd7b3
Alex Vasyliev <olorin2007@gmail.com>
2023-05-04 21:14:28 +0200

    Initial Commit

---
+++ Developers/DevelopersApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevelopersApp.swift
+//  Developers
+//
+//  Created by Oleksii Vasyliev on 04.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct DevelopersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
