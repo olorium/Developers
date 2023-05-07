5c93ca1cd658547f03afbcaee8db0a200d584744
Alex Vasyliev <olorin2007@gmail.com>
2023-05-07 20:42:53 +0200

    animation added

--- Developers/CardView.swift
+++ Developers/CardView.swift
@@ -10,11 +10,15 @@ import SwiftUI
 struct CardView: View {
 	// MARK: - Properties
 	var card: Card
+	@State private var fadeIn = false
+	@State private var moveDownward = false
+	@State private var moveUpward = false
 	
 	// MARK: - Body
     var body: some View {
 		ZStack {
 			Image(card.imageName)
+				.opacity(fadeIn ? 1.0 : 0.0)
 			
 			VStack {
 				Text(card.title)
@@ -27,7 +31,7 @@ struct CardView: View {
 					.foregroundColor(.white)
 					.italic()
 			}
-			.offset(y: -218)
+			.offset(y: moveDownward ? -218 : -300)
 			
 			Button {
 				print("")
@@ -44,13 +48,22 @@ struct CardView: View {
 			.background(LinearGradient(gradient: Gradient(colors: card.gradientColors), startPoint: .leading, endPoint: .trailing))
 			.clipShape(Capsule())
 			.shadow(color: Color("ColorShadow"), radius: 6, x: 0, y: 3)
-			.offset(y: 210)
+			.offset(y: moveUpward ? 210 : 300)
 
 		}
 		.frame(width: 355, height: 545)
 		.background(LinearGradient(colors: card.gradientColors, startPoint: .top, endPoint: .bottom))
 		.cornerRadius(16)
 		.shadow(radius: 8)
+		.onAppear() {
+			withAnimation(.linear(duration: 1.2)) {
+				fadeIn.toggle()
+			}
+			withAnimation(.linear(duration: 0.8)) {
+				moveDownward.toggle()
+				moveUpward.toggle()
+			}
+		}
     }
 }
 
